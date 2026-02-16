9772fe94ec6dcb2d3b3f76d8d088f37bb3f4ab4f
Ben Siraphob <bensiraphob@gmail.com>
2026-02-15 23:58:44 -0500

    Add blurred mock entry below public leaderboard to hint at more

--- src/App.tsx
+++ src/App.tsx
@@ -707,6 +707,10 @@ function PublicLeaderboard() {
               </div>
             </div>
           ))}
+          <div className="item blurred-item" aria-hidden="true" onClick={() => setSignupPrompt(true)}>
+            <div className="title">#4 Mysterious Player <span className="muted" style={{ marginLeft: 8 }}>??:??s</span></div>
+            <div className="meta">Sign up to see more</div>
+          </div>
         </div>
       )}
     </div>
