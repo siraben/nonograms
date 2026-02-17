71d55b580846f097c43e26a633f4d627559ff9b5
Ben Siraphob <bensiraphob@gmail.com>
2026-02-16 22:28:45 -0500

    Allow grid cells to shrink to fit, with 18px minimum floor

--- src/NonogramPlayer.tsx
+++ src/NonogramPlayer.tsx
@@ -400,7 +400,7 @@ export default function NonogramPlayer(props: {
       }
     }
 
-    return { gridTemplateColumns: `repeat(${cols}, var(--cell-size, 28px))`, cells: items };
+    return { gridTemplateColumns: `repeat(${cols}, minmax(var(--cell-min, 18px), var(--cell-size, 28px)))`, cells: items };
   }, [puzzle, state]);
 
   return (
